22eae0338a7afc0d15b1d14c73b97fc3f986a455
partridgep <paul.partridg@gmail.com>
2021-11-16 08:20:55 -0500

    updates details page

--- src/views/WeddingDetails.vue
+++ src/views/WeddingDetails.vue
@@ -7,12 +7,36 @@
     </section>
     <section id="location" class="info-section">
       <h2>{{gettingThereTitle}}</h2>
-      <p>{{gettingThereParagraph[0]}}<a :href="gmapsLinks" target="_blank">297 Shore Rd, Chatham, MA 02633</a>{{gettingThereParagraph[1]}}</p>
+      <p>{{gettingThereParagraph[0]}}<a :href="gmapsLinks" target="_blank">297 Shore Rd, Chatham, MA 02633</a>{{gettingThereParagraph[1]}}.</p>
       <Map/>
+      <h3>{{flyingTitle}}</h3>
+      <ul>
+        <li>{{flying1}}</li>
+        <li>{{flying2}}</li>
+      </ul>
+      <h3>{{trainTitle}}</h3>
+      <ul>
+        <li>{{train1}}</li>
+      </ul>
     </section>
     <section id="hotels" class="info-section">
       <h2>{{hotelsTitle}}</h2>
-      <p>{{hotelsParagraph[0]}}</p>
+      <ul>
+        <li><a :href="chathamLink" target="_blank">Chatham Bars Inn</a>: {{hotel1}}</li>
+        <li><a :href="chathamInnLink" target="_blank">Chatam Inn</a>: {{hotel2}}</li>
+        <li><a :href="chathamTidesLink" target="_blank">Chatam Tides</a>: {{hotel3}}</li>
+        <li><a :href="chathamHighlanderLink" target="_blank">Chatam Highlander Hotel</a>: {{hotel4}}</li>
+      </ul>
+    </section>
+    <section id="eat" class="info-section">
+      <h2>{{eatTitle}}</h2>
+      <ul>
+        <li><a :href="squireLink" target="_blank">Chatam Squire</a>: {{eat1}}</li>
+        <li><a :href="chathamBarsDiningLink" target="_blank">Chatam Bars Inn</a>: {{eat2}}</li>
+        <li><a :href="cuveeLink" target="_blank">Cuvée (Chatham Inn)</a>: {{eat3}}</li>
+        <li><a :href="delMarLink" target="_blank">Del Mar</a>: {{eat4}}</li>
+        <li>{{eat5}}</li>
+      </ul>
     </section>
     <section id="schedule" class="info-section">
       <h2>{{scheduleTitle}}</h2>
@@ -42,8 +66,15 @@ export default {
   data() {
     return {
       chathamLink: 'https://www.chathambarsinn.com/',
-      chathamImglink: 'https://hauteliving.com/totliving/wp-content/uploads/2019/02/girls-weekend-chatham-bars-inn.jpg',
+      chathamImglink: 'https://i.imgur.com/6C9bU3N.jpg',
       gmapsLinks: 'https://www.google.com/maps/place/297+Shore+Rd,+Chatham,+MA+02633/@41.6849731,-69.9545042,17z/data=!3m1!4b1!4m5!3m4!1s0x89fb135f29381ae5:0xd31d250630bb7b91!8m2!3d41.6849731!4d-69.9523155',
+      chathamInnLink: 'https://chathaminn.com',
+      chathamTidesLink: 'https://www.chathamtides.com',
+      chathamHighlanderLink: 'https://www.chathamhighlander.com',
+      squireLink: 'http://www.thesquire.com',
+      chathamBarsDiningLink: 'https://www.chathambarsinn.com/dining/',
+      cuveeLink: 'https://cuveechatham.com',
+      delMarLink: 'https://www.delmarbistro.com'
     }
   },
   computed: {
@@ -64,23 +95,93 @@ export default {
     },
     gettingThereTitle() {
       return this.inEnglish
-        ? 'Getting There'
+        ? 'Travel'
         : 'Y Arriver'
     },
     gettingThereParagraph() {
       return this.inEnglish
-        ? ['The Chatham Bars Inn is located in Cape Cod, at ', '. You can fly into the Cape Cod airport or drive down from Boston.']
-        : ['Le Chatham Bars Inn est situé à Cape Cod, à ', ". Vous pouvez voler jusqu'à l'aéroport de Cape Code ou arriver en voiture depuis Boston."]
+        ? ['The Chatham Bars Inn is located in Cape Cod, at ']
+        : ['Le Chatham Bars Inn est situé à Cape Cod, à ']
+    },
+    flyingTitle() {
+      return this.inEnglish
+        ? 'Flying'
+        : 'Par Avion'
+    },
+    flying1() {
+      return this.inEnglish
+        ? 'Fly into Providence Rhode Island Airport and rent a car (an easy 1hr 45m drive).'
+        : 'Arrivez à Providence Rhode Island Airport et louez une voiture (1h45 de conduite facile.)'
+    },
+    flying2() {
+      return this.inEnglish
+        ? 'Fly into Boston Logan Airport and rent a car (2hr drive).'
+        : 'Arrivez à Boston Logan Airport et louez une voiture (2h de conduite.)'
+    },
+    trainTitle() {
+      return this.inEnglish
+        ? 'Train'
+        : 'Par Train'
+    },
+    train1() {
+      return this.inEnglish
+        ? 'From NYC Penn Station take direct Amtrak train to Hyannis, MA (rent a car or rideshare for 30m drive).'
+        : "Depuis la Penn Station à New York, prenez le train direct Amtrak jusqu'à Hyannis, MA (puis louez une voiture ou prenez un Uber pour 30 minute.)"
     },
     hotelsTitle() {
       return this.inEnglish
-        ? 'Hotels'
+        ? 'Stay'
         : 'Hôtels'
     },
-    hotelsParagraph() {
+    hotel1() {
       return this.inEnglish
-        ? ['Besides the Chatham Bars Inn, there are a number of hotels conveniently located nearby. Check back soon for more details!']
-        : ["À part l'hôtel Chatham Bars Inn, il y a d'autres hôtels situés proches. Revisitez ce site bientôt pour plus d'informations!"]
+        ? 'block of rooms under the Mortell/Partridge name for Thursday and Friday nights; special block rate of $299/night.'
+        : "bloc de chambres sous le nom Mortell/Partridge pour jeudi et vendredi soirs; prix spécial de $299/nuit."
+    },
+    hotel2() {
+      return this.inEnglish
+        ? 'small, boutique hotel tucked away in the heart of Main Street; off-season rooms starting at $299/night.'
+        : "petit hôtel dans le coeur de Main Street; chambres hors-saison à partir de $299/nuit."
+    },
+    hotel3() {
+      return this.inEnglish
+        ? 'upscale, seaside hotel 14 minute drive from Chatham Bars Inn; off-season pricing starting at ~ $175.'
+        : 'hôtel au bord de la mer à 14 minutes de conduite du Chatham Bars Inn; prix hors-saison à partir de $175.'
+    },
+    hotel4() {
+      return this.inEnglish
+        ? 'newly refurbished hotel 5 minute drive from Chatham Bars Inn; off-season pricing starting at ~ $165.'
+        : 'hôtel récemment rénové à 14 minutes de conduite du Chatham Bars Inn; prix hors-saison à partir de $165.'
+    },
+    eatTitle() {
+      return this.inEnglish
+        ? 'Eat'
+        : 'Manger'
+    },
+    eat1() {
+      return this.inEnglish
+        ? 'casual pub and bar, wonderful lobster bisque and clam chowder; perfect spot for a lowkey lunch or after wedding party ;)'
+        : "bar avec bisque de homard et soupe de palourdes; endroint parfait pour un déjeuner une fête post-mariage ;)"
+    },
+    eat2() {
+      return this.inEnglish
+        ? 'upscale bar and restaurants'
+        : "bar et restaurants chics"
+    },
+    eat3() {
+      return this.inEnglish
+        ? 'fine French dining, tucked away on Main Street'
+        : "fine cuisine Française, caché sur Main Street"
+    },
+    eat4() {
+      return this.inEnglish
+        ? 'casual and delicious bistro-fare; short 6 minute drive from Chatham Bars Inn'
+        : "bistro à 6 minutes de conduite de Chatham Bars Inn"
+    },
+    eat5() {
+      return this.inEnglish
+        ? 'Coffee runs: Dunkin Donuts and Starbucks outside of town'
+        : 'Café: Starbucks en dehors de la ville'
     },
     scheduleTitle() {
       return this.inEnglish
@@ -125,7 +226,7 @@ h1 {
   margin: 5rem 0;
   padding: 0 5px;
 }
-p {
+p, ul {
   font-family: 'Josefin Sans', sans-serif;
   font-size: 1.5rem;
   font-weight: 300;
@@ -175,6 +276,15 @@ p > span {
   font-size: 2rem;
   letter-spacing: 3px;
 }
+.info-section h3 {
+  font-family: 'Playfair Display', serif;
+  font-size: 1.5rem;
+  letter-spacing: 2.5px;
+  margin-left: 2rem;
+}
+.info-section ul {
+  margin: 0 3rem;
+}
 .info-section p {
   margin-top: 4rem;
 }
@@ -183,6 +293,9 @@ p > span {
     margin: 1rem 0 5rem 0;
   }
   .info-section h2 {
+    margin: 0 2rem;
+  }
+  .info-section ul {
     margin: 0 2rem;
   }
 }
